a370c8342a51e175fc90a7ca01a612b1025ed52a
seagle <yuanxiudong66@sina.com>
2018-06-02 15:21:01 +0800

    添加希尔排序算法

---
+++ src/com/seagle/utils/algorithms/sorting/ShellSorting.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.seagle.utils.algorithms.sorting;
+
+import java.util.List;
+
+/**
+ * Shell sort algorithms.
+ *
+ * Time Complex rate: O(n^2) Space Complex rate:O(1)
+ *
+ * @author seagle
+ * @since 2018-6-2
+ */
+public class ShellSorting extends Sorting {
+
+    @Override
+    public <T extends Comparable<? super T>> void sort(List<T> data) {
+        for (int gap = data.size() / 2; gap > 0; gap /= 2) {
+            for (int group = 0; group < gap; group++) {
+                for (int index = group + gap; index < data.size(); index += gap) {
+                    T object = data.get(index);
+                    int dstIndex = index - gap;
+                    while (dstIndex >= 0 && data.get(dstIndex).compareTo(object) > 0) {
+                        data.set(dstIndex + gap, data.get(dstIndex));
+                        dstIndex -= gap;
+                    }
+                    data.set(dstIndex + gap, object);
+                }
+            }
+        }
+    }
+
+}
--- src/com/seagle/utils/algorithms/sorting/SortingAlgorithmsMain.java
+++ src/com/seagle/utils/algorithms/sorting/SortingAlgorithmsMain.java
@@ -12,15 +12,15 @@ import java.util.List;
 public class SortingAlgorithmsMain {
 
     private enum SortAlgorithmsType {
-        Bubble, Quick, Selection, Insertion,BinaryInsertion,
+        Bubble, Quick, Selection, Insertion, BinaryInsertion, Sehll,
     }
 
-    private static final Integer[] DATA = new Integer[]{2, 10, 8, 4, 3, 10, 15, 7, 4, 9, 8, 11, 41, 22, 50, 10, 6,0};
+    private static final Integer[] DATA = new Integer[]{2, 10, 8, 4, 3, 10, 15, 7, 4, 9, 8, 11, 41, 22, 50, 10, 6, 0};
 
     public static void main(String[] args) {
         List<Integer> dataList = Arrays.asList(DATA);
         long startTime = System.currentTimeMillis();
-        SortAlgorithmsType sortType = SortAlgorithmsType.BinaryInsertion;
+        SortAlgorithmsType sortType = SortAlgorithmsType.Sehll;
         Sorting sort = createSortAlgorithm(sortType);
         System.out.println("Befor sort....................");
         printArray(dataList);
@@ -41,6 +41,8 @@ public class SortingAlgorithmsMain {
                 return new InsertionSorting();
             case BinaryInsertion:
                 return new BinaryInsertionSorting();
+            case Sehll:
+                return new ShellSorting();
             default:
                 return null;
         }
